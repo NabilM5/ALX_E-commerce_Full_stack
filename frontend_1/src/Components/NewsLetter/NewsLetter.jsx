63365685d287a81e82e33a4394ad5179a4ea3571
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-12-21 01:25:10 +0300

    update

--- frontend_1/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend_1/src/Components/NewsLetter/NewsLetter.jsx
@@ -3,8 +3,13 @@ import './NewsLetter.css'
 
 export const NewsLetter = () => {
   return (
-    <div>
-        
+    <div className='newsletter'>
+      <h1>Get Exclusive Ofeers On Your Email</h1>
+      <p>Subscribe to our newletter and stay updated</p>
+      <div>
+        <input type='email' placeholder='Enter your Email' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
