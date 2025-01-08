757336b0c695e370ec3543eb2ff59da37404bb51
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2025-01-08 15:38:43 +0300

    update

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,14 @@ import './Navbar.css'
 import navlogo from '../../assets/nav-logo.svg'
 import navProfile from '../../assets/nav-profile.svg'
 
-export const Navbar = () => {
+
+const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={navlogo} alt="" className="nav-logo" />
-        <img src={navProfile} alt="" className="nav-profile" />
+      <img src={navlogo} alt="" className="nav-log" />
+      <img src={navProfile} alt="" className="nav-profile" />
     </div>
   )
 }
+
+export default Navbar
